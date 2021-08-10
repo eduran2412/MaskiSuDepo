f64c6e8fa2639258aa32525cc24b1d9f4348ffc1
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-08-10 15:04:45 +0300

    user related transactions

--- app/src/main/java/com/erenduran/maskisudepo/SignUpActivity.java
+++ app/src/main/java/com/erenduran/maskisudepo/SignUpActivity.java
@@ -3,12 +3,56 @@ package com.erenduran.maskisudepo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText usernameText,passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
+
+        usernameText = findViewById(R.id.user_name_signup_activity);
+        passwordText = findViewById(R.id.password_text_signp_activity);
+
+    }
+
+    public void signUp(View view){
+        ParseUser user = new ParseUser();
+        user.setUsername(usernameText.getText().toString());
+        user.setPassword(passwordText.getText().toString());
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null){
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"User signed up!!!",Toast.LENGTH_LONG).show();
+                    // intent (kullanıcı nereye gidecek)
+                }
+            }
+        });
+    }
+    public void signIn(View view){
+        ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e!=null){
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Welcome : " + user.getUsername(),Toast.LENGTH_LONG).show();
+                    // intent
+                }
+            }
+        });
     }
 }
