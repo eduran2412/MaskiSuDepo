020dad33be4bf7089c10203f3d87b3a56cd5c137
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-08-24 13:10:37 +0300

    place creation activity

--- app/src/main/java/com/erenduran/maskisudepo/CreatePlaceActivity.java
+++ app/src/main/java/com/erenduran/maskisudepo/CreatePlaceActivity.java
@@ -3,12 +3,32 @@ package com.erenduran.maskisudepo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CreatePlaceActivity extends AppCompatActivity {
+
+    EditText nameText,typeText,atmosphereText;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_place);
+
+        nameText = findViewById(R.id.name_text_create_place_activity);
+        typeText = findViewById(R.id.type_text_create_place_activity);
+        atmosphereText = findViewById(R.id.atmosphere_text_create_place_activity);
+        imageView = findViewById(R.id.imageview_create_place_activity);
+
+
+    }
+
+    public void next(View view){
+
+
+        // intent
     }
 }
