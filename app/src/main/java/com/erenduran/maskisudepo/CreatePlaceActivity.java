7ccbbdb9982b7357ddc48eed459761b6de9c4af2
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-08-26 15:12:57 +0300

    image selection process

--- app/src/main/java/com/erenduran/maskisudepo/CreatePlaceActivity.java
+++ app/src/main/java/com/erenduran/maskisudepo/CreatePlaceActivity.java
@@ -1,16 +1,29 @@
 package com.erenduran.maskisudepo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
 
 public class CreatePlaceActivity extends AppCompatActivity {
 
     EditText nameText,typeText,atmosphereText;
     ImageView imageView;
+    Bitmap chosenImage;
 
 
     @Override
@@ -24,6 +37,40 @@ public class CreatePlaceActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageview_create_place_activity);
 
 
+    }
+    public void selectPicture(View view){
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},2);
+        } else{
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode ==2){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+            try {
+                chosenImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+                imageView.setImageBitmap(chosenImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void next(View view){
