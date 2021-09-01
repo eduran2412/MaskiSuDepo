02bf21683d43d722511d26d254f3ef5fc368e7dc
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-09-01 09:25:42 +0300

    pull details from server

--- app/src/main/java/com/erenduran/maskisudepo/DetailActivity.java
+++ app/src/main/java/com/erenduran/maskisudepo/DetailActivity.java
@@ -3,16 +3,23 @@ package com.erenduran.maskisudepo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
@@ -68,6 +75,38 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
                 }else{
                     if(objects.size()>0){
 
+                        for(ParseObject object : objects){// objects içinde bir for loop yap
+
+                            ParseFile parseFile = (ParseFile) object.get("image");
+                            // arka planda bu datayı download et
+                            parseFile.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] data, ParseException e) {
+                                    if(e == null && data != null){
+
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
+                                        imageView.setImageBitmap(bitmap);
+
+                                        nameText.setText(placeName);
+                                        typeText.setText(object.getString("type"));
+                                        atmosphereText.setText(object.getString("atmosphere"));
+
+                                        latitudeString = object.getString("latitude");
+                                        longitudeString = object.getString("longitude");
+
+                                        latitude = Double.parseDouble(latitudeString);
+                                        longitude = Double.parseDouble(longitudeString);
+
+                                        mMap.clear();
+
+                                        LatLng placeLocation = new LatLng(latitude,longitude);
+                                        mMap.addMarker(new MarkerOptions().position(placeLocation).title(placeName));
+                                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(placeLocation,15));
+
+                                    }
+                                }
+                            });
+                        }
                     }
                 }
             }
